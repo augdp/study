26ba62fa709add2d9b55c385e9f3f3bb785dd119
augdp <augusto.dp@hotmail.com>
2026-06-22 13:21:24 -0300

    feat: TUI skeleton; some mock data

--- lib/data.sh
+++ lib/data.sh
@@ -1,1 +1,33 @@
 #!/usr/bin/env bash
+
+data_list_summaries() {
+  printf '%s\n' \
+    "The Brothers Karamazov" \
+    "Moby-Dick" \
+    "The Iliad"
+}
+
+data_list_sections() {
+  local summary="$1"
+
+  case "$summary" in
+    "The Brothers Karamazov")
+      printf '%s\n' \
+        "Book I - A Nice Little Family" \
+        "Book II - An Inappropriate Gathering" \
+        "Book III - The Sensualists"
+      ;;
+    "Moby-Dick")
+      printf '%s\n' \
+        "Chapter 1 - Loomings" \
+        "Chapter 2 - The Carpet-Bag" \
+        "Chapter 3 - The Spouter-Inn"
+      ;;
+    "The Iliad")
+      printf '%s\n' \
+        "Book 1 - The Rage of Achilles" \
+        "Book 2 - The Great Gathering" \
+        "Book 3 - Helen Reviews the Champions"
+      ;;
+  esac
+}
--- lib/screens.sh
+++ lib/screens.sh
@@ -9,17 +9,97 @@ screen_main() {
       "Thinking (not available yet)" \
       "Quit" |
     gum choose
-)"
+  )"
 
-case "$choice" in
-  "Reading")
-    action_not_implemented "Reading screen"
-    ;;
-  "Thinking (not available yet)")
-    action_not_implemented "Thinking"
-    ;;
-  "Quit"|"")
-    exit 0
-    ;;
+  case "$choice" in
+    "Reading")
+      screen_reading
+      ;;
+    "Thinking (not available yet)")
+      action_not_implemented "Thinking"
+      ;;
+    "Quit"|"")
+      exit 0
+      ;;
+  esac
+}
+
+screen_reading() {
+  ui_title "Reading"
+
+  choice="$(
+    printf '%s\n' \
+      "Reopen last session" \
+      "See books" |
+    gum choose
+  )"
+
+  case "$choice" in
+    "Reopen last session")
+      action_not_implemented "Reopen last session"
+      ;;
+    "See books")
+      screen_books
+      ;;
+    "")
+      exit 0
+      ;;
+  esac
+}
+
+screen_books() {
+  ui_title "Books"
+
+  summary="$(data_list_summaries | gum choose)"
+
+  case "$summary" in
+    "")
+      exit 0
+      ;;
+    *)
+      screen_summary "$summary"
+      ;;
+  esac
+}
+
+screen_summary() {
+  local summary="$1"
+
+  ui_title "$summary"
+
+  section="$(data_list_sections "$summary" | gum choose)"
+
+  case "$section" in
+    "")
+      exit 0
+      ;;
+    *)
+      screen_section "$summary" "$section"
+      ;;
+  esac
+}
+
+screen_section() {
+  local summary="$1"
+  local section="$2"
+
+  ui_title "$summary / $section"
+
+  choice="$(
+    printf '%s\n' \
+      "Generate section files" \
+      "Review text for section audio" \
+      "Generate section audio" \
+      "Launch reading session" |
+    gum choose
+  )"
+
+  case "$choice" in
+    "")
+      exit 0
+      ;;
+    *)
+      action_not_implemented "$choice"
+      ;;
   esac
 }
